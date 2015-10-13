3056000f70b8172620b821d212ef22c230449612
svett89 <axel.naucler@gmail.com>
2015-10-13 21:29:45 +0200

    v 0.1.01 - påbörjade tester och kvittoklass
    Har börjat lägga till tester för kvittoklassen, har börjat implementera
    vissa funktioner i kvitto - valt en datastruktur (HashMap) för att spara
    varor, lagt till några metoder.

--- Kassasystem/src/kassa/Kvitto.java
+++ Kassasystem/src/kassa/Kvitto.java
@@ -1,5 +1,47 @@
 package kassa;
 
-public class Kvitto {
+import java.util.*;
 
+public class Kvitto {
+	private HashMap<Vara, Integer> varuMap = new HashMap<Vara, Integer>();
+	
+	public Kvitto(){
+		//Tom konstruktor, bör behållas?
+	}
+	public Kvitto(Vara... varor){
+		for(Vara v : varor){
+			varuMap.put(v, (varuMap.get(v)+1));
+		}
+	}
+	
+	public void läggTillVara(Vara v, int antal){
+		varuMap.put(v, (varuMap.get(v)+antal));
+	}
+	
+	public void läggTillVara(Vara v){
+		varuMap.put(v, (varuMap.get(v)+1));
+	}
+	
+	public boolean varaFinns(Vara v){
+		return varuMap.containsKey(v) && varuMap.get(v) > 0;
+	}
+	
+	public int getTotalMängdVaror(){
+		Collection<Integer> totalMängd = varuMap.values();
+		int total = 0;
+		for(Integer i : totalMängd){
+			total += i;
+		}
+		return total;
+	}
+	
+	public void läggTillVarorFrånSamling(Collection<Vara> varuSamling){
+		for(Vara v : varuSamling){
+			varuMap.put(v, (varuMap.get(v)+1));
+		}
+	}
+	
+	public Set<Vara> getVaruSet(){
+		return varuMap.keySet();
+	}
 }
--- Kassasystem/src/kassa/Vara.java
+++ Kassasystem/src/kassa/Vara.java
@@ -1,5 +1,5 @@
 package kassa;
 
 public class Vara {
-
+//MÅSTE FÅ EN ORDENTLIG HASHCODE!
 }
