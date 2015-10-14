380c5037aa9a8188d531552325fcddc502c6a38f
barisakdag <barisakdag91@hotmail.com>
2015-10-14 22:38:46 +0200

    Kod till Tester v1

--- Kassasystem/src/kassa/Pengar.java
+++ Kassasystem/src/kassa/Pengar.java
@@ -1,5 +1,63 @@
 package kassa;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.math.*;
 public class Pengar {
 
+    private BigDecimal belopp;
+    private Currency valuta;
+    private RoundingMode avrundningsMode;
+    
+    
+    public Pengar(BigDecimal belopp, Currency valuta, RoundingMode avrundningsMode){
+        
+        this.belopp = belopp;
+        this.valuta = valuta;
+        this.avrundningsMode = avrundningsMode;
+  
+    }
+    public Pengar(BigDecimal belopp){
+        this.belopp=belopp;
+    }
+    
+
+    public BigDecimal getBelopp(){
+        return belopp;
+    }
+    
+    public Currency getValuta(){
+        return valuta;
+    }
+    
+    public RoundingMode getAvrundningsMode(){
+        return avrundningsMode;
+    }
+
+    public boolean ärNegativBelopp() {
+        return belopp.compareTo(BigDecimal.ZERO)< 0;
+    }
+
+    public boolean ärPositivBelopp() {
+        return belopp.compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    public boolean ärNoll() {
+        return belopp.compareTo(BigDecimal.ZERO) == 0;
+    }
+    public void gånger(Pengar p2) {
+        // TODO Auto-generated method stub
+        
+    }
+    public void minus(Pengar p2) {
+        // TODO Auto-generated method stub
+        
+    }
+    public void plus(Pengar p2) {
+        // TODO Auto-generated method stub
+        
+    }
+   
+    
+    
 }
--- Kassasystem/src/kassa/Pengartest.java
+++ Kassasystem/src/kassa/Pengartest.java
@@ -1,6 +1,5 @@
 package kassa;
 import static org.junit.Assert.*;
-
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Currency;
@@ -8,67 +7,86 @@ import java.util.Currency;
 import org.junit.Test;
 
 
-public class PengarTest {
+public class Pengartest {
 
+    // Skapa pengar med bara bigDecimal B
+    private Pengar skapaPengar(BigDecimal b){
+        return new Pengar(b);
+    }
+    
+    
     BigDecimal belopp = new BigDecimal("3266.52");
-    Money testPengar = new Money(belopp,Currency.getInstance("EUR"),RoundingMode.UP);   
+    Pengar testPengar = new Pengar(belopp,Currency.getInstance("EUR"),RoundingMode.UP);   
   
     
-    // Använder här Currency klassens to.string för att se om 
+    // Använder här Currency klassens to.string för att se om valutan man skickar in stämmer, PASS
     @Test
-    public void testValuta() {
+    public void testaValuta() {
         assertEquals("EUR",testPengar.getValuta().toString());
     }
 
     // Testar beloppet, Bigdecimal kan inte sättas in i expected parametern så man får skapa den innan.
     @Test
-    public void testBelopp() {
+    public void testaBelopp() {
         BigDecimal expected = new BigDecimal("3266.52");
         assertEquals(testPengar.getBelopp(),expected);
     }
     
     @Test
-    public void testAvrundingStil(){
+    public void testaAvrundingStil(){
         assertEquals(testPengar.getAvrundningsMode(),RoundingMode.UP);
     }
     
     @Test
-    public void testBeloppÖverNoll(){
-        assertEquals(testPengar.isPositivBelopp(),true);
+    public void testaBeloppÖverNoll(){
+        assertEquals(testPengar.ärPositivBelopp(),true);
     }
     
     @Test
-    public void testBeloppUnderNoll(){
-        assertEquals(testPengar.isNegativBelopp(),false);
+    public void testaBeloppUnderNoll(){
+        assertEquals(testPengar.ärNegativBelopp(),false);
     }
     
     @Test
-    public void testNollPunkt(){
-        BigDecimal zero = new BigDecimal("0");
-        Money testAmountZero = new Money(zero,Currency.getInstance("SEK"),RoundingMode.UP);
-        assertEquals(testAmountZero.isNoll(),true);
+    public void testaNollPunkt(){
+        BigDecimal noll = new BigDecimal("0");
+        Pengar testaPengarNoll = new Pengar(noll,Currency.getInstance("SEK"),RoundingMode.UP);
+        assertEquals(testaPengarNoll.ärNoll(),true);
     }
     
     //Ej klar
     @Test
     public void testAdderaPengar(){
+        Pengar p1 = skapaPengar(BigDecimal.valueOf(2.0));
+        Pengar p2 = skapaPengar(BigDecimal.valueOf(3.0));
+        
+        p1.plus(p2);
+        BigDecimal expected = new BigDecimal("5.0");
+        assertEquals(p1.getBelopp(), expected);
         
     }
     
     @Test
     public void testSubtraheraPengar(){
-        
+        Pengar p1 = skapaPengar(BigDecimal.valueOf(5.0));
+        Pengar p2 = skapaPengar(BigDecimal.valueOf(3.0));
+       
+        p1.minus(p2);
+        BigDecimal expected = new BigDecimal("2.0");
+        assertEquals(p1.getBelopp(), expected);
     }
     @Test
     public void testMultipliceraPengar(){
+        Pengar p1 = skapaPengar(BigDecimal.valueOf(5.0));
+        Pengar p2 = skapaPengar(BigDecimal.valueOf(3.0));
+        
+        p1.gånger(p2);
+       
+        BigDecimal expected = new BigDecimal("15.0");
+        assertEquals(p1.getBelopp(), expected);
         
     }
-    
-    @Test
-    public void testSummaAvPengar(){
-        
-    }
-    
+  
 }
 
 
