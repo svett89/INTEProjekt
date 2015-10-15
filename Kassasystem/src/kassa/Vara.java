235f23c44a4695b18b7a3e137af5b202c9e1aa77
svett89 <axel.naucler@gmail.com>
2015-10-15 15:50:28 +0200

    Revert "Merge remote-tracking branch 'refs/remotes/origin/Kvitto+Kvittotest'"
    This reverts commit 8bc7382dc9d5a2d2b7e1722c8d9fe5600c8a7f95, reversing
    changes made to 3ff931beccc52c251faf73789b8601cd6fb59a72.

--- Kassasystem/src/kassa/Vara.java
+++ Kassasystem/src/kassa/Vara.java
@@ -1,57 +1,5 @@
 package kassa;
-import java.math.*;
 
 public class Vara {
-	private String namn;
-	//Ändra till pengar
-	private Pengar pris;
-
-	public Vara(String namn, Pengar pris) {
-		//Kollar inte maxvärde! Fixa!
-		if (pris.ärNegativBelopp()) {
-			throw new IllegalArgumentException();
-		}
-		this.namn = namn;
-		this.pris = pris;
-	}
-
-	public String getNamn() {
-		return namn;
-	}
-	//Ändra till pengar
-	public Pengar getPris() {
-		return pris;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((namn == null) ? 0 : namn.hashCode());
-		result = prime * result + ((pris == null) ? 0 : pris.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (!(obj instanceof Vara))
-			return false;
-		Vara other = (Vara) obj;
-		if (namn == null) {
-			if (other.namn != null)
-				return false;
-		} else if (!namn.equals(other.namn))
-			return false;
-		if (pris == null) {
-			if (other.pris != null)
-				return false;
-		} else if (!pris.equals(other.pris))
-			return false;
-		return true;
-	}
 
 }
