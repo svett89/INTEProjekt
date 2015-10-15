763fd2ce5725d52c1168e2bd68905d2cb6264c58
svett89 <axel.naucler@gmail.com>
2015-10-15 14:33:47 +0200

    v 0.1.03
    Lagt till vara och varatester (temporära)

--- Kassasystem/src/kassa/Vara.java
+++ Kassasystem/src/kassa/Vara.java
@@ -1,36 +1,53 @@
 package kassa;
 
 public class Vara {
-	private String testDescription;
-	public Vara(String testDescription){
-		this.testDescription = testDescription;
+	private String namn;
+	private double pris;
+
+	public Vara(String namn, double pris) {
+
+		if (pris < 0 || pris > 9999) {
+			throw new IllegalArgumentException();
+		}
+		this.namn = namn;
+		this.pris = pris;
 	}
+
+	public String getNamn() {
+		return namn;
+	}
+
+	public double getPris() {
+		return pris;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result
-				+ ((testDescription == null) ? 0 : testDescription.hashCode());
+		result = prime * result + ((namn == null) ? 0 : namn.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(pris);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
 			return false;
-		if (!(obj instanceof Vara))
+		if (getClass() != obj.getClass())
 			return false;
 		Vara other = (Vara) obj;
-		if (testDescription == null) {
-			if (other.testDescription != null)
+		if (namn == null) {
+			if (other.namn != null)
 				return false;
-		} else if (!testDescription.equals(other.testDescription))
+		} else if (!namn.equals(other.namn))
+			return false;
+		if (Double.doubleToLongBits(pris) != Double.doubleToLongBits(other.pris))
 			return false;
 		return true;
-	}
-	
-	public String toString(){
-		return testDescription;
 	}
 }
--- Kassasystem/src/kassa/Varatest.java
+++ Kassasystem/src/kassa/Varatest.java
@@ -1,7 +1,32 @@
 package kassa;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
-public class Varatest extends TestCase {
+public class Varatest {
 
+	@Test
+	public void skapaOchTestaEnVara() {
+		Vara v1 = new Vara("Salami", 22);
+		assertTrue(v1.getNamn() == "Salami");
+		assertTrue(v1.getPris() == 22);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void kastarExceptionNärPrisFörStor() {
+		Vara v1 = new Vara("Bröd", 10000);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void kastarExceptionNärPrisÄrMinus() {
+		Vara v1 = new Vara("Bröd", -2);
+	}
+
+	@Test
+	public void LikaHashCodeOchEquals() {
+		Vara v1 = new Vara("Ost", 59.99);
+		Vara v2 = new Vara("Ost", 59.99);
+		assertTrue(v1.hashCode() == v2.hashCode());
+		assertTrue(v1.equals(v2));
+	}
 }
