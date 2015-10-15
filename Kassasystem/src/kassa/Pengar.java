0633f69f5287937cf5c45e73b8cff843a3e1deb7
svett89 <axel.naucler@gmail.com>
2015-10-15 14:36:33 +0200

    v 0.1.04
    La till pengarbranchen

--- Kassasystem/src/kassa/Pengar.java
+++ Kassasystem/src/kassa/Pengar.java
@@ -1,5 +1,64 @@
 package kassa;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.math.*;
+
 public class Pengar {
 
+    private BigDecimal belopp;
+    private Currency valuta;
+    private RoundingMode avrundningsMode;
+    
+    
+    public Pengar(BigDecimal belopp, Currency valuta, RoundingMode avrundningsMode){
+        
+        this.belopp = belopp;
+        this.valuta = valuta;
+        this.avrundningsMode = avrundningsMode;
+  
+    }
+    public Pengar(BigDecimal belopp){
+        this.belopp=belopp;
+    }
+    
+
+    public BigDecimal getBelopp(){
+        return belopp;
+    }
+    
+    public Currency getValuta(){
+        return valuta;
+    }
+    
+    public RoundingMode getAvrundningsMode(){
+        return avrundningsMode;
+    }
+
+    public boolean ärNegativBelopp() {
+        return belopp.compareTo(BigDecimal.ZERO)< 0;
+    }
+
+    public boolean ärPositivBelopp() {
+        return belopp.compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    public boolean ärNoll() {
+        return belopp.compareTo(BigDecimal.ZERO) == 0;
+    }
+    public Pengar gånger(int gånger) {
+        BigDecimal faktor = new BigDecimal(gånger);
+        return new Pengar(belopp.multiply(faktor));
+    }
+    public Pengar minus(Pengar p2) {
+        return new Pengar(belopp.subtract(p2.belopp));
+        
+    }
+    public Pengar plus(Pengar p2) {
+        return new Pengar(belopp.add(p2.belopp));
+    }
+  
+    public String toString(){
+        return belopp.toPlainString();
+      }
 }
--- Kassasystem/src/kassa/Pengartest.java
+++ Kassasystem/src/kassa/Pengartest.java
@@ -1,7 +1,87 @@
 package kassa;
+import static org.junit.Assert.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Currency;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
-public class Pengartest extends TestCase {
 
+public class Pengartest {
+
+    // Skapa pengar med bara bigDecimal B
+    private Pengar skapaPengar(BigDecimal b){
+        return new Pengar(b);
+    }
+    
+    
+    BigDecimal belopp = new BigDecimal("3266.52");
+    Pengar testPengar = new Pengar(belopp,Currency.getInstance("EUR"),RoundingMode.UP);   
+  
+    
+    // Använder här Currency klassens to.string för att se om valutan man skickar in stämmer, PASS
+    @Test
+    public void testaValuta() {
+        assertEquals("EUR",testPengar.getValuta().toString());
+    }
+
+    // Testar beloppet, Bigdecimal kan inte sättas in i expected parametern så man får skapa den innan.
+    @Test
+    public void testaBelopp() {
+        BigDecimal expected = new BigDecimal("3266.52");
+        assertEquals(expected,testPengar.getBelopp());
+    }
+    
+    @Test
+    public void testaAvrundingStil(){
+        assertEquals(RoundingMode.UP, testPengar.getAvrundningsMode());
+    }
+    
+    @Test
+    public void testaBeloppÖverNoll(){
+        assertEquals(true, testPengar.ärPositivBelopp());
+    }
+    
+    @Test
+    public void testaBeloppUnderNoll(){
+        assertEquals(false, testPengar.ärNegativBelopp());
+    }
+    
+    @Test
+    public void testaNollPunkt(){
+        BigDecimal noll = new BigDecimal("0");
+        Pengar testaPengarNoll = new Pengar(noll,Currency.getInstance("SEK"),RoundingMode.UP);
+        assertEquals(true, testaPengarNoll.ärNoll());
+    }
+    
+    //Ej klar
+    @Test
+    public void testAdderaPengar(){
+        Pengar p1 = skapaPengar(BigDecimal.valueOf(2.0));
+        Pengar p2 = skapaPengar(BigDecimal.valueOf(3.0));
+        
+        assertEquals(new BigDecimal("5.0"), p1.plus(p2).getBelopp());
+        
+    }
+    
+    @Test
+    public void testSubtraheraPengar(){
+        Pengar p1 = skapaPengar(BigDecimal.valueOf(5.0));
+        Pengar p2 = skapaPengar(BigDecimal.valueOf(3.0));
+       
+        //p1.minus(p2).getBelopp();
+
+        assertEquals(new BigDecimal("2.0"), p1.minus(p2).getBelopp() );
+    }
+    @Test
+    public void testMultipliceraPengar(){
+        Pengar p1 = skapaPengar(BigDecimal.valueOf(5.0));
+        int faktor = 3;
+        assertEquals(new BigDecimal("15.0"), p1.gånger(faktor).getBelopp());
+        
+    }
+  
+    
 }
+
+
