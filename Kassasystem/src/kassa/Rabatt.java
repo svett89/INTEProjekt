58f726f8cedc24677f0dc64bd8759e624b56a1a0
Johnny Huhtala <Animzor@gmail.com>
2015-10-16 01:19:21 +0200

    Nytt försök utifrån UML-diagrammet

--- Kassasystem/src/kassa/Rabatt.java
+++ Kassasystem/src/kassa/Rabatt.java
@@ -2,4 +2,8 @@ package kassa;
 
 public class Rabatt {
 
+	public static void addVara(Vara vara) {
+		
+	}
+
 }
--- Kassasystem/src/kassa/Rabattest.java
+++ Kassasystem/src/kassa/Rabattest.java
@@ -1,7 +1,24 @@
 package kassa;
 
-import junit.framework.TestCase;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.math.BigDecimal;
 
-public class Rabattest extends TestCase {
+public class Rabattest {
+	
+	Kvitto k1 = new Kvitto();
+	Pengar p1 = new Pengar(new BigDecimal(100));
+	Pengar p2 = new Pengar(new BigDecimal(155));
+	Pengar p3 = new Pengar(new BigDecimal(288));
+	Vara v1 = new Vara("cykel", p1);
+	Vara v2 = new Vara("bil", p2);
+	Vara v3 = new Vara("stol", p3);
+	
+	@Test
+	public void testaOmVarorKanLäggasTill(){
+		Rabatt.addVara(v1);
+		assertEquals(Rabatt);
+	}
+	
 
 }
